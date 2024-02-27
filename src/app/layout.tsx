53668ed69a22878868471ed69389429b5ab84b53
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-02-27 09:57:10 +0100

    lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,10 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body         className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div className="p-4">
-      <p className="text-xl font-bold">App</p>
+        <p className="text-xl font-bold">App</p>
       </div>
       <Separator />
     </main>
