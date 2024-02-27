5ef9217a297a41d6d94f0fe88d1bfcdcf4ff3b6e
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-02-27 09:34:48 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "@/components/ui/separator";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="p-4">
+      <p className="text-xl font-bold">App</p>
+      </div>
+      <Separator />
+    </main>
+  );
+}
