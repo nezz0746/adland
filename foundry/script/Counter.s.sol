9a7a03dcdedaa8a141428ad32e4354ea87fc54a6
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-02-28 10:34:38 +0100

    deploy Counter on sepolia

--- foundry/script/Counter.s.sol
+++ foundry/script/Counter.s.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+import {BaseScript} from "./Base.s.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract CounterScript is BaseScript {
+    function deploySepolia() public broadcastOn(DeployementChain.Sepolia) {
+        Counter counter = new Counter();
 
-    function run() public {
-        vm.broadcast();
+        _saveDeployment(address(counter), "Counter");
     }
 }
