2e8a4cea312fa094b9c8c687aba0dd5e1a9712c2
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-04 12:34:27 +0100

    base buyFromListing call

--- foundry/test/Listing.t.sol
+++ foundry/test/Listing.t.sol
@@ -8,6 +8,7 @@ import {WETH9} from "../src/mocks/WETH9.sol";
 import {DirectListingsLogic} from "contracts/prebuilts/marketplace/direct-listings/DirectListingsLogic.sol";
 import {AdGroupFactory} from "../src/ListingFactory.sol";
 import {MarketplaceV3} from "contracts/prebuilts/marketplace/entrypoint/MarketplaceV3.sol";
+import {CurrencyTransferLib} from "contracts/lib/CurrencyTransferLib.sol";
 
 contract ListingTest is ListingBase {
     WETH9 public weth;
@@ -43,6 +44,26 @@ contract ListingTest is ListingBase {
 
         assertEq(adGroupFactory.getAdGroup(1).size, 3);
     }
+
+    function testBuyListing() public {
+        _grantListRole(address(adGroupFactory));
+
+        adGroupFactory.createAdGroup(3);
+
+        address buyer = vm.addr(69);
+
+        vm.deal(buyer, 1);
+
+        marketplace.buyFromListing{value: 0.1 ether}(
+            1,
+            buyer,
+            1,
+            CurrencyTransferLib.NATIVE_TOKEN,
+            0.1 ether
+        );
+    }
+
+    ////////////////////////// HELPERS //////////////////////////
 
     function _grantListRole(address to) internal {
         vm.prank(deployer);
