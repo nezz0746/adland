e123e0743865321b90d88400dd9a8a2812382d6f
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-10 00:22:41 +0100

    add superfluid constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 import { ENTRYPOINT_ADDRESS_V06 } from "permissionless";
-import { Chain } from "viem";
+import { Address, Chain } from "viem";
 import { sepolia } from "viem/chains";
 
 export const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "";
@@ -19,3 +19,16 @@ export const pimilcoURLV2 =
   "https://api.pimlico.io/v2/sepolia/rpc?apikey=" + pimlicoAPIKey;
 export const pimilcoURLV1 =
   "https://api.pimlico.io/v1/sepolia/rpc?apikey=" + pimlicoAPIKey;
+
+export const superfluidAddresses: Record<
+  11155111,
+  { ethx: Address; daix: Address; cfaV1: Address }
+> = {
+  [11155111]: {
+    ethx: "0x30a6933Ca9230361972E413a15dC8114c952414e",
+    daix: "0x9Ce2062b085A2268E8d769fFC040f6692315fd2c",
+    cfaV1: "0x6836F23d6171D74Ef62FcF776655aBcD2bcd62Ef",
+  },
+};
+
+export const NATIVE_CURRENCY = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
