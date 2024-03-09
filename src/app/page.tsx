992c882a93fc5aada67fecf3a670ac4e7f1b61a2
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-09 20:05:28 +0100

    feat: list ad groups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,39 @@
 
 import { ConnectButton } from "@/components/connect-button";
 import { Separator } from "@/components/ui/separator";
-import { useSmartAccount } from "@/lib/pimlico";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useReadAdCommonOwnershipGetAllGroups } from "@/generated";
+import { zeroAddress } from "viem";
 
 export default function Home() {
-  const { smartAccount } = useSmartAccount();
+  const { data: adGroups } = useReadAdCommonOwnershipGetAllGroups({
+    args: [BigInt(1), BigInt(10)],
+    query: {
+      select: (data) => {
+        return data
+          .filter((adGroup) => adGroup.beneficiary !== zeroAddress)
+          .map((adGroup) => {
+            return {
+              beneficiary: adGroup.beneficiary,
+              startListingId: adGroup.startListingId,
+              endListingId: adGroup.endListingId,
+              size: Number(adGroup.endListingId - adGroup.startListingId),
+            };
+          });
+      },
+    },
+  });
+
+  console.log({ adGroups });
 
   return (
     <>
@@ -15,7 +44,37 @@ export default function Home() {
       </nav>
       <Separator />
       <main className="p-4 flex flex-col gap-2">
-        {smartAccount && <p>{smartAccount.account.address}</p>}
+        <Table>
+          <TableCaption>All Perpetual Ad Groups.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Group ID</TableHead>
+              <TableHead>Beneficiary</TableHead>
+              <TableHead></TableHead>
+              <TableHead className="text-right">Size</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {adGroups?.map(
+              ({ beneficiary, startListingId, endListingId, size }, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{index}</TableCell>
+                  <TableCell>{beneficiary}</TableCell>
+                  <TableCell>{""}</TableCell>
+                  <TableCell className="text-right">{size}</TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">
+                {adGroups?.reduce((acc, adGroup) => acc + adGroup.size, 0)}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
       </main>
     </>
   );
