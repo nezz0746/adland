1bbb7bddd7d486a435146c31b953f30eaf4839f1
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-10 17:16:55 +0100

    feat: add upgrade & downgrade util buttons on home smart account card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -10,11 +19,46 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useReadAdCommonOwnershipGetAllGroups } from "@/generated";
+import {
+  isethAbi,
+  useReadAdCommonOwnershipGetAllGroups,
+  useReadSuperTokenBalanceOf,
+} from "@/generated";
+import { superfluidAddresses } from "@/lib/constants";
+import { useSmartAccount } from "@/lib/pimlico";
 import { useRouter } from "next/navigation";
-import { zeroAddress } from "viem";
+import {
+  Address,
+  encodeFunctionData,
+  formatEther,
+  parseEther,
+  zeroAddress,
+} from "viem";
+import { useBalance, useSendTransaction } from "wagmi";
 
 export default function Home() {
+  const { smartAccount } = useSmartAccount();
+
+  const { data: balanceData } = useBalance({
+    address: smartAccount?.account.address,
+
+    query: {
+      enabled: Boolean(smartAccount?.account.address),
+      refetchInterval: 10000,
+    },
+  });
+
+  const { data: ethXBalance } = useReadSuperTokenBalanceOf({
+    address: superfluidAddresses[11155111].ethx,
+    args: [smartAccount?.account?.address as Address],
+    query: {
+      enabled: Boolean(smartAccount?.account.address),
+      refetchInterval: 10000,
+    },
+  });
+
+  const { sendTransaction } = useSendTransaction({});
+
   const { push } = useRouter();
 
   const { data: adGroups } = useReadAdCommonOwnershipGetAllGroups({
@@ -40,6 +84,69 @@ export default function Home() {
 
   return (
     <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Account</CardTitle>
+          <CardDescription>
+            Your account and balance. Use the buttons to fund your account and
+            upgrade your ETHx.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{smartAccount?.account.address}</p>
+          <div className="flex flex-row justify-between">
+            <p>{Number(formatEther(balanceData?.value ?? BigInt(0)))} ETH</p>
+            <p>{Number(formatEther(ethXBalance ?? BigInt(0)))} ETHx</p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex flex-row justify-between">
+          <Button
+            onClick={() => {
+              if (!smartAccount?.account.address) return;
+              sendTransaction({
+                to: smartAccount?.account.address,
+                value: parseEther("0.1"),
+              });
+            }}
+          >
+            Fund 0.1 ETH
+          </Button>
+          <div className="flex flex-row gap-2">
+            <Button
+              onClick={() => {
+                if (!smartAccount?.account.address) return;
+                smartAccount.sendTransaction({
+                  to: superfluidAddresses[11155111].ethx,
+                  value: parseEther("0.01"),
+                  data: encodeFunctionData({
+                    abi: isethAbi,
+                    functionName: "upgradeByETH",
+                    args: undefined,
+                  }),
+                });
+              }}
+            >
+              Upgrade 0.01 ETHx
+            </Button>
+            <Button
+              onClick={() => {
+                if (!smartAccount?.account.address || !ethXBalance) return;
+                smartAccount.sendTransaction({
+                  to: superfluidAddresses[11155111].ethx,
+                  // value: ethXBalance,
+                  data: encodeFunctionData({
+                    abi: isethAbi,
+                    functionName: "downgradeToETH",
+                    args: [ethXBalance],
+                  }),
+                });
+              }}
+            >
+              Downgrade All
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
       <Table>
         <TableCaption>All Perpetual Ad Groups.</TableCaption>
         <TableHeader>
