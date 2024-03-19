dd198e109cdc0aa19d2cafea523b0801e4a6c256
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-19 11:50:48 +0100

    feat: add basic farcaster signin with input & publish button

--- src/app/group/[id]/distribution/page.tsx
+++ src/app/group/[id]/distribution/page.tsx
@@ -1,5 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { usePrivy } from "@privy-io/react-auth";
+import { useState } from "react";
+
 const GroupDistributionPage = () => {
-  return <p>Distribution Page</p>;
+  const { user, linkFarcaster } = usePrivy();
+  const [channelName, setChannelName] = useState("");
+
+  const signedInWithFarcaster = Boolean(user?.farcaster);
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Farcaster Channel Distribution</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Input
+            type="text"
+            value={channelName}
+            onChange={(e) => setChannelName(e.target.value)}
+            placeholder="Channel Name"
+          />
+        </CardContent>
+        <CardFooter className="gap-2">
+          <Button
+            disabled={signedInWithFarcaster}
+            className="gap-2"
+            onClick={() => {
+              if (!signedInWithFarcaster) linkFarcaster();
+            }}
+          >
+            <img
+              src={
+                signedInWithFarcaster
+                  ? user?.farcaster?.pfp ?? "/farcaster.png"
+                  : "/farcaster.png"
+              }
+              width={20}
+              height={20}
+              alt="farcaster logo"
+            />
+            {signedInWithFarcaster
+              ? user?.farcaster?.username
+              : "Sign in with Farcaster"}
+          </Button>
+          <Button disabled={!signedInWithFarcaster}>Publish Ads</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default GroupDistributionPage;
