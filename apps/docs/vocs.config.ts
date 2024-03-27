981b975e537fb74179ff1cc0d8b47cc8bfb61349
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-27 10:09:11 +0100

    add use cases docs

--- apps/docs/vocs.config.ts
+++ apps/docs/vocs.config.ts
@@ -7,5 +7,19 @@ export default defineConfig({
       text: "What is Adland ?",
       link: "/what-is-adland",
     },
+    {
+      text: "Use cases",
+      collapsed: true,
+      items: [
+        {
+          text: "Offchain: Display Ads",
+          link: "/use-cases/web-ads",
+        },
+        {
+          text: "Offchain: Social Media Ads",
+          link: "/use-cases/social-media-ads",
+        },
+      ],
+    },
   ],
 });
